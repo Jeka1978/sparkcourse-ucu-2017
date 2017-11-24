0ce99064d05a2d8cb8499390b3237172f6c08a75
bsevgeny@gmail.com <papakita2009>
2017-11-24 11:07:36 +0200

    log file was added

--- src/main/java/taxi_lab/Main.java
+++ src/main/java/taxi_lab/Main.java
@@ -1,11 +1,15 @@
 package taxi_lab;
 
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
 /**
  * @author Evgeny Borisov
  * @since 3.2
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println();
+
     }
 }
