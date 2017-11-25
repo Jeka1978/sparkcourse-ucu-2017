8f4d40fd4d56638bc4b76e8c8ddfd4fef3bcf22c
bsevgeny@gmail.com <papakita2009>
2017-11-25 11:46:18 +0200

    dataframes continue

--- src/main/java/linkedIn/Main.java
+++ src/main/java/linkedIn/Main.java
@@ -2,16 +2,21 @@ package linkedIn;
 
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaSparkContext;
-import org.apache.spark.sql.Column;
-import org.apache.spark.sql.DataFrame;
-import org.apache.spark.sql.SQLContext;
-import org.apache.spark.sql.functions;
+import org.apache.spark.sql.*;
+import org.apache.spark.sql.types.StructField;
+
+import static org.apache.spark.sql.functions.*;
 
 /**
  * @author Evgeny Borisov
  * @since 3.2
  */
 public class Main {
+    private static final String SALARY = "salary";
+    private static final String AGE = "age";
+    private static final String KEYWORDS = "keywords";
+    private static final String KEYWORD = "keyword";
+
     public static void main(String[] args) {
         SparkConf sparkConf = new SparkConf();
         sparkConf.setMaster("local");
@@ -24,10 +29,37 @@ public class Main {
         dataFrame.show();
         dataFrame.printSchema();
 
+        StructField[] fields = dataFrame.schema().fields();
+        for (StructField field : fields) {
+            System.out.println(field.dataType());
+        }
 
-        dataFrame = dataFrame.withColumn("age+10", functions.col("age").plus(10));
+
+        dataFrame = dataFrame.withColumn(SALARY,
+                col(AGE).multiply(10).multiply(size(col(KEYWORDS))));
         dataFrame.show();
+
+        DataFrame keyWordDF = dataFrame.withColumn(KEYWORD, explode(col(KEYWORDS))).select(KEYWORD);
+
+
+        Row row = keyWordDF.groupBy(KEYWORD).agg(count(KEYWORD).as("amount"))
+                .sort(col("amount").desc()).first();
+//        keyWordDF.groupBy(KEYWORD).count().show();
+
+        String mostPopular = row.getAs(KEYWORD);
+        System.out.println("mostPopular = " + mostPopular);
+
+        dataFrame.filter(col(SALARY).leq(1200))
+                .filter(array_contains(col(KEYWORDS),mostPopular)).show();
 
 
     }
 }
+
+
+
+
+
+
+
+
